1afc54f9b76b77ca77c830ea23ff19a6622ada85
adrian <adrianribeiroprofissional.com>
2026-02-23 16:30:33 -0300

    atualizando layout do modal

--- src/pages/dashBord.ts
+++ src/pages/dashBord.ts
@@ -125,14 +125,34 @@ export const dashBord = function plate(){
                         </svg>
                         <h1>Dados do Cliente<h1> 
                     </div> 
-                    <div class="flex flex-col bg-sky-900 rounded-xl p-5 gap-3">
-                        <div class="flex flex-col justify-center border-b border-gray-300 gap-2">
-                            <div>Nome do Cliente</div>
-                            <div>Telefone</div>
+                    <div class="grid grid-cols-1 bg-sky-900 rounded-xl p-5 gap-3 sm:grid-cols-2">
+                        <div class="grid grid-cols-1 justify-center border-b border-gray-300 gap-2 p-1 sm:border-r-2 sm:border-b-0 md:grid-cols-2 md:border-r-0">
+                            <div class="flex gap-2 items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                                </svg>
+                                <h1>Adrian Raul Ribeiro</h1>
+                            </div>
+                            <div class="flex gap-2 items-center" >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                                </svg>
+                                <h1>47991076484</h1>
+                            </div>
                         </div>
-                        <div class="flex flex-col justify-center gap-2">
-                            <div>Placa</div>
-                            <div>Status</div>
+                        <div class="grid grid-cols-1 justify-center gap-2 md:grid-cols-2">
+                            <div class="flex gap-2 p-1 items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                                </svg>
+                                <h1>MLR5B18</h1>
+                            </div>
+                            <div class="p-1 flex gap-2 items-center" >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                                <h1>Pendente</h1>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -243,9 +263,9 @@ export const dashBord = function plate(){
                         <h1 class="text-white">Confirmar Vistoria</h1>
                     </div>
                     <div  class="flex items-center gap-2 bg-red-600/25 rounded-xl p-2 cursor-pointer" >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 stroke-red-500">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 stroke-red-500">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
                         <h1>Cancelar Vistoria</h1>
                     </div>
                 </div>
