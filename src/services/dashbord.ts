c0d3b3d36f5ad09fa348d0666c0f2f5ac7bb5f72
adrian <adrianribeiroprofissional.com>
2026-02-24 16:18:00 -0300

    atualizando tudo

--- src/services/dashbord.ts
+++ src/services/dashbord.ts
@@ -319,6 +319,43 @@ export function dataModal(){
         
         // Fotos Vistoria
 
+            const areaVist = document.createElement('div')
+            
+            const areaVistTitle = document.createElement('div')
+            const svgVistTitle = document.createElement('div')
+            const h1VistTitle = document.createElement('h1')
+            
+            const areaVistPhoto = document.createElement('div')
+            const vistPhoto = document.createElement('div')
+            const imgVistPhoto = document.createElement('img')
+            const h1VistPhoto = document.createElement('h1')
+        
+        //classes
+            areaVist.classList.add('flex', 'flex-col', 'p-5', 'gap-5')
+
+            areaVistTitle.classList.add('flex', 'gap-3', 'items-center')
+            svgVistTitle.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4"><path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" /></svg>'
+            h1VistTitle.innerHTML = "Fotos da Vistoria"
+
+            areaVistPhoto.classList.add('grid', 'grid-cols-1', 'overflow-y-scroll', )
+            vistPhoto.classList.add('flex', 'flex-col', 'items-center', 'bg-gray-700', 'w-30', 'h-30', 'rounded-2xl','cursor-pointer')
+            imgVistPhoto.classList.add( 'h-20')
+            
+
+            h1VistPhoto.innerHTML="Frente"
+        
+        //Estrutura
+
+            areaVistTitle.appendChild(svgVistTitle)
+            areaVistTitle.appendChild(h1VistTitle)
+
+          
+            vistPhoto.appendChild(imgVistPhoto)
+            vistPhoto.appendChild(h1VistPhoto)
+            areaVistPhoto.appendChild(vistPhoto)
+
+            areaVist.appendChild(areaVistTitle)
+            areaVist.appendChild(areaVistPhoto)
 
         // ações
             const areaAcoe = document.createElement('div')
@@ -375,6 +412,7 @@ export function dataModal(){
             mae.appendChild(cabecalho);
             mae.appendChild(bordaCabecalho);
             mae.appendChild(dateClientArea);
+            mae.appendChild(areaVist)
             mae.appendChild(areaAcoe)
 
         //estrutura do modal
