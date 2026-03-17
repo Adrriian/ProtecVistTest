ac66bbfb5c92bc0efa6aee3b0a16d18385e6d89d
adrian <adrianribeiroprofissional.com>
2026-03-17 13:02:34 -0300

    asdasd

--- src/pages/dashbord-de-clientes/dashbordClientes.tsx
+++ src/pages/dashbord-de-clientes/dashbordClientes.tsx
@@ -45,7 +45,7 @@ export function DashboardClientes() {
             <div className="container md:min-h-full lg:flex lg:min-h-full area_blur ">
                 <header id="bar" className="min-h-full  bg-slate-900 z-99 absolute p-5 w-0 -ml-10 overflow-hidden transition-all ease-linear duration-500 lg:flex lg:flex-col lg:static lg:w-85 lg:min-h-screen lg:ml-0">
                     <nav>
-                        <div className="flex items-center border-b border-gray-400 pb-4 gap-5 ml-10">
+                        <div className=" ola flex items-center border-b border-gray-400 pb-4 gap-5 ml-10">
                             <div className="flex items-center bg-blue-700 p-2 rounded">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 stroke-white">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
