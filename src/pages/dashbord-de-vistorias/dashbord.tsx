62ff57f3eac9eeb8092d2768244729d3dd956a4a
adrian <adrianribeiroprofissional.com>
2026-03-17 13:21:46 -0300

    asd

--- src/pages/dashbord-de-vistorias/dashbord.tsx
+++ src/pages/dashbord-de-vistorias/dashbord.tsx
@@ -61,27 +61,27 @@ export function Dashboard() {
                             </div>
                         </div>
                         <div className="mt-5 ml-10 flex flex-col gap-5">
-                            <div className="bg-blue-700 p-2 rounded flex gap-2 cursor-pointer transition-all duration-300 hover:bg-violet-700 ease-linear hover:shadow-[0_0_20px_blue]">
+                            <div onClick={() => navigateToRegister(navigate)} className="bg-blue-700 p-2 rounded flex gap-2 cursor-pointer transition-all duration-300 hover:bg-violet-700 ease-linear hover:shadow-[0_0_20px_blue]">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 stroke-white">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
                                 </svg>
-                                <h1 onClick={() => navigateToRegister(navigate)} id="link" className="cursor-pointer font-bold text-white ">Cadastrar Vistoria</h1>
+                                <h1  id="link" className="cursor-pointer font-bold text-white ">Cadastrar Vistoria</h1>
                             </div>
-                            <div className="bg-blue-700 p-2 rounded flex gap-2 cursor-pointer transition-all duration-300  hover:bg-violet-700 hover:shadow-[0_0_20px_blue] ease-linear">
+                            <div onClick={() => navigateToUserClientes(navigate)} className="bg-blue-700 p-2 rounded flex gap-2 cursor-pointer transition-all duration-300  hover:bg-violet-700 hover:shadow-[0_0_20px_blue] ease-linear">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 stroke-white">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 </svg>
 
-                                <h1 onClick={() => navigateToUserClientes(navigate)} id="link" className="cursor-pointer font-bold text-white ">Associados</h1>
+                                <h1  id="link" className="cursor-pointer font-bold text-white ">Associados</h1>
                             </div>
-                            <div className="bg-blue-700 p-2 rounded flex gap-2 cursor-pointer transition-all duration-300  hover:bg-violet-700 hover:shadow-[0_0_20px_blue] ease-linear">
+                            <div onClick={() => navigateToUserSettings(navigate)} className="bg-blue-700 p-2 rounded flex gap-2 cursor-pointer transition-all duration-300  hover:bg-violet-700 hover:shadow-[0_0_20px_blue] ease-linear">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 stroke-white">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 </svg>
 
-                                <h1 onClick={() => navigateToUserSettings(navigate)} id="link" className="cursor-pointer font-bold text-white ">Configurações</h1>
+                                <h1  id="link" className="cursor-pointer font-bold text-white ">Configurações</h1>
                             </div>
                         </div>
                     </nav>
