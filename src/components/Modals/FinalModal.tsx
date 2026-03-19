89fda8c85fd2466357b60ba72e9e9442351e4806
adrian <adrianribeiroprofissional.com>
2026-03-19 16:54:59 -0300

    layout da parte final finalizada

--- src/components/Modals/FinalModal.tsx
+++ src/components/Modals/FinalModal.tsx
@@ -6,17 +6,27 @@ export default function FinalModal({ fotos }: any) {
   }
 
   return (
-    <div className="p-5 text-center">
-      <h2 className="mb-4">Finalizar</h2>
+    <div className="min-h-screen flex flex-col items-center justify-center p-5 ">
+      <div className="w-75 bg-white p-5 rounded flex items-center justify-center flex-col gap-5">
+        <h2 className="mb-4 text-violet-700 font-bold text-xl">Finalizar</h2>
 
-      <p>{fotos.length} fotos capturadas</p>
+        <div className="flex gap-1 max-w-75  flex-wrap">
+          {fotos.map((item: any, index: number) => (
+            <img className="w-15 rounded-xl"
+              key={index}
+              src={item.preview} />)
+          )}
+        </div>
 
-      <button
-        onClick={enviar}
-        className="bg-blue-500 text-white px-4 py-2 mt-4 rounded"
-      >
-        Enviar
-      </button>
+
+        <button
+          onClick={enviar}
+          className="bg-blue-700 text-white px-4 py-2 mt-4 rounded hover:px-5 hover:py-3 cursor-pointer hover:bg-violet-700 transition-all duration-300 ease-linear hover:shadow-[0px_0px_10px_blue]"
+        >
+          Enviar
+        </button>
+      </div>
+
     </div>
   );
 }
