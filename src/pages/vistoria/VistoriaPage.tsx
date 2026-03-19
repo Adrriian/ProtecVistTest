cfcf5b2386c7d02860349c4d042e30dd842fd226
adrian <adrianribeiroprofissional.com>
2026-03-19 10:17:57 -0300

    testando denovo

--- src/pages/vistoria/VistoriaPage.tsx
+++ src/pages/vistoria/VistoriaPage.tsx
@@ -1,7 +1,7 @@
 
 import { useVistoria } from "../../hooks/useVistoria";
-import {IntroModal} from "../../components/Modals/introModal";
-import {TipoModal} from "../../components/Modals/tipoModal";
+import {IntroModal} from "../../components/Modals/introModalsExe";
+import {TypeModals} from "../../components/Modals/typesModalsExes";
 import GuiaFotoModal from "../../components/Modals/GuiaFotoModal";
 import FinalModal from "../../components/Modals/FinalModal";
 
@@ -11,7 +11,7 @@ export function VistoriaPage() {
   return (
     <>
       {vistoria.etapa === "intro" && <IntroModal {...vistoria} />}
-      {vistoria.etapa === "tipo" && <TipoModal {...vistoria} />}
+      {vistoria.etapa === "tipo" && <TypeModals {...vistoria} />}
       {vistoria.etapa === "guia" && <GuiaFotoModal {...vistoria} />}
       {vistoria.etapa === "final" && <FinalModal {...vistoria} />}
     </>
